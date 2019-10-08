63da50adbef11f51ddb4f8fcf39084f56e26edfa
panterasls <panterasls123@gmail.com>
2019-10-08 17:05:21 +0300

    Initial Commit

---
+++ TestPrjAttract/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestPrjAttract
+//
+//  Created by Надежда Возна on 08.10.2019.
+//  Copyright © 2019 Надежда Возна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
