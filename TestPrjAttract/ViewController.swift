c3e3dbacc9f333622def6532042707edbd3d4009
panterasls <panterasls123@gmail.com>
2019-10-09 14:17:49 +0300

    added comments

--- TestPrjAttract/ViewController.swift
+++ TestPrjAttract/ViewController.swift
@@ -13,18 +13,21 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     var heroes = [Heroes]()
 
+    // MARK: - viewDidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getData()
         self.tableView.dataSource = self
     }
-
+    
+    // MARK: - parse JSON
+    
     func getData() {
         guard let url = URL(string: "http://test.php-cd.attractgroup.com/test.json") else { return }
         let session = URLSession.shared
         session.dataTask(with: url) { (data, _, error) in
             guard let data = data else { return }
-            
             do {
                 let decoder = JSONDecoder()
                 self.heroes = try decoder.decode([Heroes].self, from: data)
@@ -37,9 +40,13 @@ class ViewController: UIViewController, UITableViewDataSource {
         }.resume()
     }
 
+    // MARK: - numberOfRowsInSection
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return heroes.count
     }
+    
+    // MARK: - cellForRowAt
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCell
@@ -49,5 +56,4 @@ class ViewController: UIViewController, UITableViewDataSource {
 
         return cell
     }
-
 }
