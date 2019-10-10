dca348d0b8ecc331820faf63c6c6c92ad0fa0dd5
panterasls <panterasls123@gmail.com>
2019-10-10 15:00:12 +0300

    restore menu

---
+++ TestPrjAttract/MenuTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MenuTableViewController.swift
+//  TestPrjAttract
+//
+//  Created by Надежда Возна on 10.10.2019.
+//  Copyright © 2019 Надежда Возна. All rights reserved.
+//
+
+import UIKit
+
+enum MenuType: Int {
+    case main
+    case list
+    case settings
+}
+
+class MenuTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        guard let menuType = MenuType(rawValue: indexPath.row) else { return }
+        dismiss(animated: true)
+    }
+    
+}
