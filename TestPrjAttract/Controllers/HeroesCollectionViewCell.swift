1670b9b61c181a9c2ca3cc48528ccae2c6582ed7
panterasls <panterasls123@gmail.com>
2019-10-12 16:23:02 +0300

    created slider

---
+++ TestPrjAttract/Controllers/HeroesCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  HeroesCollectionViewCell.swift
+//  TestPrjAttract
+//
+//  Created by Надежда Возна on 12.10.2019.
+//  Copyright © 2019 Надежда Возна. All rights reserved.
+//
+
+import UIKit
+
+class HeroesCollectionViewCell: UICollectionViewCell {
+    
+}
